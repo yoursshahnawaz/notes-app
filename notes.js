981886bf222ec9df718efe83e1a0079cae0bf662
unknown <shahnawazgalaxy340@gmail.com>
2020-01-26 22:55:33 +0530

    Added files in repo

--- notes.js
+++ notes.js
@@ -5,8 +5,6 @@ const addNote = (title, body) => {
     const notes = loadNotes()
     const duplicateNote = notes.find((note) => note.title === title)
 
-    debugger
-
     if(!duplicateNote){
         notes.push({
             title: title,
@@ -20,59 +18,7 @@ const addNote = (title, body) => {
     }
 }
 
-const removeNote = (title) => {
-    const notes = loadNotes()
-    const notesToKeep = notes.filter((note) => note.title !== title)
-
-    if(notes.length > notesToKeep.length){
-        saveNotes(notesToKeep)
-        console.log(chalk.green.inverse('Notes removed!'))
-    }
-    else{
-        console.log(chalk.red.inverse('No note found!'))
-    }
-}
-
-const listNotes = () => {
-    const notes = loadNotes()
-    console.log(chalk.inverse('Your Notes'))
-    notes.forEach((note) => {
-        console.log(note.title)
-    })
-}
-
-const readNote = (title) => {
-    const notes = loadNotes()
-    const note = notes.find((note) => note.title === title)
-
-    if(note){
-        console.log(chalk.inverse(note.title))
-        console.log(note.body)
-    }
-    else{
-        console.log(chalk.red.inverse('Note not found!'))
-    }
-}
-
-const saveNotes = (notes) => {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json',dataJSON)
-}
-
-const loadNotes = () => {
-    try{
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
-    }
-    catch(e){
-        return []
-    }
-}
 
 module.exports = {
-    addNote: addNote,
-    removeNote: removeNote,
-    listNotes: listNotes,
-    readNote: readNote
+    addNote: addNote
 }
