8e119ea6287e73619edfd622e15441360967fe60
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-01-26 23:26:21 +0530

    added notes operations
    Added removeNote, listNotes and readNote functionalities

--- notes.js
+++ notes.js
@@ -18,6 +18,41 @@ const addNote = (title, body) => {
     }
 }
 
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title)
+
+    if(notes.length > notesToKeep.length){
+        saveNotes(notesToKeep)
+        console.log(chalk.green.inverse('Notes removed!'))
+    }
+    else{
+        console.log(chalk.red.inverse('No note found!'))
+    }
+}
+
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.inverse('Your Notes'))
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if(note){
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    }
+    else{
+        console.log(chalk.red.inverse('Note not found!'))
+    }
+}
+
+
 const loadNotes = () => {
     try{
         const dataBuffer = fs.readFileSync('notes.json')
