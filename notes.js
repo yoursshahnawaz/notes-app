812592ec445ab0a0324866cae8736ea442dc49b9
unknown <shahnawazgalaxy340@gmail.com>
2020-01-26 23:04:29 +0530

    added loadNotes & saveNotes

--- notes.js
+++ notes.js
@@ -19,6 +19,22 @@ const addNote = (title, body) => {
 }
 
 
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json',dataJSON)
+}
+
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    }
+    catch(e){
+        return []
+    }
+}
+
 module.exports = {
     addNote: addNote
 }
